1aaccf7e3750d53dbd0b1ee9705d9ea2236639c1
marco balamon <marco.balamon0506@gmail.com>
2023-05-18 22:13:35 +0200

    site's style remade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,32 +10,8 @@ import { User } from './Modele/User';
 export class AppComponent {
   title = 'frontend-webProject-2023';
   static showContent = true;
-  constructor(_userService: UserService){}
-
-  getUser(){
-    return UserService._user; 
-  }
 
   showContent(){
     return AppComponent.showContent
   }
-  canGo(loc: string ){
-  
-
-    if(UserService._user!= null){
-      
-      if(UserService._user.type === 'Admin')
-        return true
-      if(loc === 'home')
-        return true 
-      if(loc === 'manage user')
-        return false 
-      if(loc === 'Client Projects' && UserService._user.type === 'Rapporteur')
-         return true
-      if(loc === 'Project status' && UserService._user.type === 'Dev'  || UserService._user.type ==='Client')
-      return true
-    }
-   
-    return false;
-}
 }
